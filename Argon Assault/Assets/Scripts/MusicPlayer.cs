a5d59f8c6f3a5d1faaaf5b1ee640c482550cd781
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-04-21 19:47:00 -0400

    Finished lesson 95. Project Tidying Checklist

--- Argon Assault/Assets/Scripts/MusicPlayer.cs
+++ Argon Assault/Assets/Scripts/MusicPlayer.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour {
-  [SerializeField] float waitTime = 3f;
-
   private void Awake() {
     int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
     if (numMusicPlayers > 1) {
@@ -14,13 +11,5 @@ public class MusicPlayer : MonoBehaviour {
     else {
       DontDestroyOnLoad(gameObject);
     }
-  }
-
-  void Start() {
-    Invoke("LoadFirstScene", waitTime);
-  }
-
-  private void LoadFirstScene() {
-    SceneManager.LoadScene(1);
   }
 }
---
+++ Argon Assault/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+  [SerializeField] float waitTime = 3f;
+
+  void Start() {
+    Invoke("LoadFirstScene", waitTime);
+  }
+
+  private void LoadFirstScene() {
+    SceneManager.LoadScene(1);
+  }
+}
