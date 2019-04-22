c9bd80a42dd61f45710bddf27d5a4c327db3b396
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-04-21 23:17:07 -0400

    Finished lesson 101. Making Scripts Add Components

---
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+  private void Start() {
+    AddNonTriggerBoxCollider();
+  }
+
+  // creating our own collider at run-time for 3rd party assets helps to
+  // make sure that our script doesn't break in case the components get updated
+  private void AddNonTriggerBoxCollider() {
+    Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+    boxCollider.isTrigger = false;
+  }
+
+  private void OnParticleCollision(GameObject other) {
+    Destroy(gameObject);
+  }
+}
